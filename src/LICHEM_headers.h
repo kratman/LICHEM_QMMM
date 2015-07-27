8edac6975f721f3b8e8c466fbc9063aa72268660
kratman <kratmandu@gmail.com>
2015-07-27 00:26:25 -0400

    Changed structs to classes

--- src/LICHEM_headers.h
+++ src/LICHEM_headers.h
@@ -134,87 +134,91 @@ int QMTime = 0; //Sum of QM wrapper times
 int MMTime = 0; //Sum of MM wrapper times
 
 //Custom data types
-struct Coord
+class Coord
 {
-  double x; //x position
-  double y; //y position
-  double z; //z position
+  public:
+    double x; //x position
+    double y; //y position
+    double z; //z position
 };
 
-struct Mpole
+class Mpole
 {
   //Cartesian multipoles
-  //Atoms for the local frame of reference
-  bool ChiralFlip; //Flip y axis
-  string Type; //Bisector, Z-then-X, Z-Only, 3-Fold, or Z-Bisect
-  int Atom1; //Atom which defines the z axis
-  int Atom2; //Atom which defines the x axis
-  int Atom3; //Atom which defines the y axis (chiral only)
-  //Monopole moment
-  double q;
-  //Cartesian dipole moments
-  double Dx;
-  double Dy;
-  double Dz;
-  //Cartesian induced dipole moments (global frame)
-  double IDx;
-  double IDy;
-  double IDz;
-  //Cartesian quadrupole moments (Q_ij = Q_ji)
-  double Qxx;
-  double Qxy;
-  double Qxz;
-  double Qyy;
-  double Qyz;
-  double Qzz;
+  public:
+    //Atoms for the local frame of reference
+    bool ChiralFlip; //Flip y axis
+    string Type; //Bisector, Z-then-X, Z-Only, 3-Fold, or Z-Bisect
+    int Atom1; //Atom which defines the z axis
+    int Atom2; //Atom which defines the x axis
+    int Atom3; //Atom which defines the y axis (chiral only)
+    //Monopole moment
+    double q;
+    //Cartesian dipole moments
+    double Dx;
+    double Dy;
+    double Dz;
+    //Cartesian induced dipole moments (global frame)
+    double IDx;
+    double IDy;
+    double IDz;
+    //Cartesian quadrupole moments (Q_ij = Q_ji)
+    double Qxx;
+    double Qxy;
+    double Qxz;
+    double Qyy;
+    double Qyz;
+    double Qzz;
 };
 
-struct RedMpole
+class RedMpole
 {
   //Reduced multipole from sph. harm. and diagonalization
-  //Monopole
-  double Q00;
-  //Dipole moments
-  double Q10; //Z component
-  double Q11c; //X component
-  double Q11s; //Y component
-  //Quadrupole moments
-  double Q20; //Z^2 component
-  double Q22c; //X^2-Y^2 component
-  //Spherical harmonic vectors
-  Vector3d Vecx; //X direction in quadrupole frame
-  Vector3d Vecy; //Y direction in quadrupole frame
-  Vector3d Vecz; //Z direction in quadrupole frame
+  public:
+    //Monopole
+    double Q00;
+    //Dipole moments
+    double Q10; //Z component
+    double Q11c; //X component
+    double Q11s; //Y component
+    //Quadrupole moments
+    double Q20; //Z^2 component
+    double Q22c; //X^2-Y^2 component
+    //Spherical harmonic vectors
+    Vector3d Vecx; //X direction in quadrupole frame
+    Vector3d Vecy; //Y direction in quadrupole frame
+    Vector3d Vecz; //Z direction in quadrupole frame
 };
 
-struct OctCharges
+class OctCharges
 {
   //A grid of point-charges which replaces multipoles
-  double q1; //Charge in the +x direction
-  double q2; //Charge in the +y direction
-  double q3; //Charge in the +z direction
-  double q4; //Charge in the -x direction
-  double q5; //Charge in the -y direction
-  double q6; //Charge in the -z direction
-  //Positions of the charges in the global frame
-  double x1;
-  double y1;
-  double z1;
-  double x2;
-  double y2;
-  double z2;
-  double x3;
-  double y3;
-  double z3;
-  double x4;
-  double y4;
-  double z4;
-  double x5;
-  double y5;
-  double z5;
-  double x6;
-  double y6;
-  double z6;
+  public:
+    double q1; //Charge in the +x direction
+    double q2; //Charge in the +y direction
+    double q3; //Charge in the +z direction
+    double q4; //Charge in the -x direction
+    double q5; //Charge in the -y direction
+    double q6; //Charge in the -z direction
+    //Positions of the charges in the global frame
+    double x1;
+    double y1;
+    double z1;
+    double x2;
+    double y2;
+    double z2;
+    double x3;
+    double y3;
+    double z3;
+    double x4;
+    double y4;
+    double z4;
+    double x5;
+    double y5;
+    double z5;
+    double x6;
+    double y6;
+    double z6;
 };
 
 class GauDen1s
@@ -246,71 +250,84 @@ class GauDen1s
     double TwoCoul(GauDen1s,double); //Density-density Coulomb repulsion
 };
 
-struct QMMMAtom
+class QMMMAtom
 {
   //Data type for atomic information
-  double m; //Mass of atom
-  bool QMregion; //QM, MM, pseudo-bond, or boundary-atom
-  bool MMregion; //QM, MM, pseudo-bond, or boundary-atom
-  bool PBregion; //QM, MM, pseudo-bond, or boundary-atom
-  bool BAregion; //QM, MM, pseudo-bond, or boundary-atom
-  bool Frozen; //Part of a frozen shell
-  string QMTyp; //Real atom type
-  string MMTyp; //Force field atom type
-  int NumTyp; //Numerical atom type (if used)
-  int NumClass; //Numerical atom class (if used)
-  int id; //Atom number, starts at zero
-  vector<int> Bonds; //Connectivity
-  double Ep; //Storage for PI energies
-  vector<Coord> P; //Array of beads
-  vector<Mpole> MP; //Multipoles
-  vector<OctCharges> PC; //Point-charge multipoles
+  public:
+    //Temporary storage
+    double Ep; //Path-integral energies
+    //Regions
+    bool QMregion; //QM, MM, pseudo-bond, or boundary-atom
+    bool MMregion; //QM, MM, pseudo-bond, or boundary-atom
+    bool PBregion; //QM, MM, pseudo-bond, or boundary-atom
+    bool BAregion; //QM, MM, pseudo-bond, or boundary-atom
+    bool Frozen; //Part of a frozen shell
+    //Force field information
+    double m; //Mass of atom
+    string QMTyp; //Real atom type
+    string MMTyp; //Force field atom type
+    int NumTyp; //Numerical atom type (if used)
+    int NumClass; //Numerical atom class (if used)
+    int id; //Atom number, starts at zero
+    vector<int> Bonds; //Connectivity
+    //Coordinates
+    vector<Coord> P; //Array of beads
+    //Multipoles
+    vector<Mpole> MP; //Multipoles
+    vector<OctCharges> PC; //Point-charge multipoles
+    //Frozen density
+    vector<GauDen1s> G1s; //Spherical electron density
 };
 
-struct QMMMElec
+class QMMMElec
 {
   //Data type for electronic information (eFF model)
-  string typ; //Lepton type
-  double m; //mass (amu)
-  double q; //Charge (au)
-  int spin; //Spin
-  double Ep; //Temporary energy for parallel
-  vector<Coord> P; //Bead coordinates
-  vector<double> rad; //Radius (Ang)
+  public:
+    //Temporary storage
+    double Ep; //Path-integral energies
+    //Particle properties
+    string typ; //Lepton type
+    double m; //mass (amu)
+    double q; //Charge (au)
+    int spin; //Spin
+    //Coordinates
+    vector<Coord> P; //Bead coordinates
+    vector<double> rad; //Radius (Ang)
 };
 
-struct QMMMSettings
+class QMMMSettings
 {
   //Input needed for QM wrappers
-  string Func; //DFT functional
-  string Basis; //Basis set for QM calculations
-  string RAM; //Ram for QM calculations
-  bool MemMB; //Is the RAM in mb or gb
-  string Charge; //QM total charge
-  string Spin; //QM total spin
-  //Input needed for MC and MD functions
-  string Ensemble; //NVT or NPT
-  double Temp; //Temperature
-  double Beta; //Inverse temperature
-  double Press; //External pressure
-  int Neq; //Number of equilibration run steps
-  int Nsteps; //Number of production run steps
-  int Nbeads; //Number of time-slices or beads
-  double accratio; //Target acceptance ratio
-  int Nprint; //Number of steps before printing
-  double dt; //MD timestep
-  double tautemp; //Thermostat time constant
-  double taupress; //Barostat time constant
-  //Input needed for optimizations
-  int MaxOptSteps; //Maximum iterative optimization steps
-  double MMOptTol; //Criteria to end the optimization
-  double QMOptTol; //Criteria to end the optimization
-  double StepScale; //Steepest descent step size (Ang)
-  double MaxStep; //Maximum size of the optimization step
-  //Input needed for reaction paths
-  double Kspring; //Elastic band spring constant
-  //Storage of energies (PIMC)
-  double Eold;
+  public:
+    string Func; //DFT functional
+    string Basis; //Basis set for QM calculations
+    string RAM; //Ram for QM calculations
+    bool MemMB; //Is the RAM in mb or gb
+    string Charge; //QM total charge
+    string Spin; //QM total spin
+    //Input needed for MC and MD functions
+    string Ensemble; //NVT or NPT
+    double Temp; //Temperature
+    double Beta; //Inverse temperature
+    double Press; //External pressure
+    int Neq; //Number of equilibration run steps
+    int Nsteps; //Number of production run steps
+    int Nbeads; //Number of time-slices or beads
+    double accratio; //Target acceptance ratio
+    int Nprint; //Number of steps before printing
+    double dt; //MD timestep
+    double tautemp; //Thermostat time constant
+    double taupress; //Barostat time constant
+    //Input needed for optimizations
+    int MaxOptSteps; //Maximum iterative optimization steps
+    double MMOptTol; //Criteria to end the optimization
+    double QMOptTol; //Criteria to end the optimization
+    double StepScale; //Steepest descent step size (Ang)
+    double MaxStep; //Maximum size of the optimization step
+    //Input needed for reaction paths
+    double Kspring; //Elastic band spring constant
+    //Storage of energies (PIMC)
+    double Eold;
 };
 
 //Function declarations (alphabetical)
