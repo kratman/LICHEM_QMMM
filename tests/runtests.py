33239effa9593ef386ff7350a57dd7ea58cd5404
kratman <kratmandu@gmail.com>
2015-08-02 00:02:20 -0400

    Test updates

---
+++ tests/runtests.py
@@ -0,0 +1,33 @@
+#### LICHEM test suite ####
+
+### PSI4 tests ###
+
+# TINKER #
+
+
+# LAMMPS #
+
+
+# AMBER #
+
+
+### Gaussian tests ###
+
+# TINKER #
+
+
+# LAMMPS #
+
+
+# AMBER #
+
+
+### NWChem tests ###
+
+# TINKER #
+
+
+# LAMMPS #
+
+
+# AMBER #
